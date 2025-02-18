84d10b635bee4a591cbe827d44d44dc75e7b000d
NelsonJDAS <nelsondeabreu4@gmail.com>
2025-02-18 19:16:38 +0100

    aniamcion guitarras

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <>
             <Header />
 
-            <main className="container-xl mt-5">
+            <main className="container-fluid w-90 mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
--- src/Components/Guitar.jsx
+++ src/Components/Guitar.jsx
@@ -1,22 +1,24 @@
-const Guitar = ({nombre, img, descripcion, precio}) => {
+const Guitar = ({ nombre, img, descripcion, precio }) => {
     return (
-        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div className="col-4">
-                <img
-                    className="img-fluid"
-                    src={img}
-                    alt="imagen guitarra"
-                />
-            </div>
-            <div className="col-8">
-                <h3 className="text-black fs-4 fw-bold text-uppercase">{nombre}</h3>
-                <p>
-                    {descripcion}
-                </p>
-                <p className="fw-black text-primary fs-3">{precio}</p>
-                <button type="button" className="btn btn-dark w-100">
-                    Agregar al Carrito
-                </button>
+        <div className="col-4 my-4 align-items-center">
+            <div className="row animacion-guitarra px-2 py-2">
+                <div className="col-4">
+                    <img
+                        className="img-fluid"
+                        src={img}
+                        alt="imagen guitarra"
+                    />
+                </div>
+                <div className="col-8">
+                    <h3 className="text-black fs-4 fw-bold text-uppercase text-center">{nombre}</h3>
+                    <p className="text-center">
+                        {descripcion}
+                    </p>
+                    <p className="fw-black text-primary fs-3 text-center precio">{precio}</p>
+                    <button type="button" className="btn btn-dark w-100 rounded-pill">
+                        Agregar al Carrito
+                    </button>
+                </div>
             </div>
         </div>
     );
